7c6a1cc383483bdfb5b5643210ca331e076f68be
Justine Romero <thepiesaresquared@gmail.com>
2021-08-04 23:00:25 +0800

    feat: lexer

---
+++ src/lexer/index.ts
@@ -0,0 +1,52 @@
+import { RuleSet } from "./rules";
+import { Token } from "./tokenizer";
+
+/**
+ * State during lexing
+ */
+export interface LexerState {
+  line_count: number;
+}
+
+/**
+ * Lexer class
+ */
+export class Lexer {
+  constructor(private readonly ruleset: RuleSet) {}
+
+  /**
+   * Lex text
+   * @param src source text
+   */
+  lex(src: string): Token[] {
+    const tokens: Token[] = [];
+    const state: LexerState = {
+      line_count: 0,
+    };
+
+    while (src) {
+      let matched = false;
+      for (const rule of this.ruleset) {
+        if (rule.will_enforce(state)) {
+          let token: Token;
+          let new_lines: number;
+          if (({ token, new_lines } = rule.tokenize(src))) {
+            src = src.substring(token.raw.length);
+            state.line_count += new_lines;
+            tokens.push(token);
+            matched = true;
+            break;
+          }
+        }
+      }
+      if (!matched)
+        throw new SyntaxError(
+          `No matching keyword for "${src.slice(0, 10)}" at line ${
+            state.line_count
+          }`
+        );
+    }
+
+    return tokens;
+  }
+}
---
+++ src/lexer/rules.ts
@@ -0,0 +1,58 @@
+import { LexerState } from ".";
+import { Token } from "./tokenizer";
+
+export type RuleSet = Rule[];
+
+interface RuleOptions {
+  token_type: string;
+  tokenizer: (src: string) => Token;
+  guards: ((state: LexerState) => void)[];
+  flags: {
+    override_guard: boolean;
+  };
+}
+
+export class Rule {
+  private options: RuleOptions;
+  constructor(opts: {
+    token_type: string;
+    tokenizer: (src: string) => Token;
+    guards?: ((state: LexerState) => void)[];
+    flags?: {
+      override_guard?: boolean;
+    };
+  }) {
+    if (!opts.guards) opts.guards = [];
+    if (!opts.flags)
+      opts.flags = {
+        override_guard: false,
+      };
+    // Add default guard. Reserve first line for title.
+    if (!opts.flags.override_guard) {
+      opts.flags.override_guard = false;
+      opts.guards.push((x) => x.line_count != 0);
+    }
+
+    this.options = {
+      token_type: opts.token_type,
+      tokenizer: opts.tokenizer,
+      guards: opts.guards,
+      flags: {
+        override_guard: opts.flags.override_guard,
+      },
+    };
+  }
+
+  will_enforce(state: LexerState) {
+    return this.options.guards.every((x) => x(state));
+  }
+
+  tokenize(src: string) {
+    const token = this.options.tokenizer(src);
+    const new_lines = (token.raw.match(/\r\n|\r|\n/g) || "").length;
+    return {
+      token,
+      new_lines,
+    };
+  }
+}
